f0adf553e806cffa1262a544c71e5a7ba92bf637
Sergio Nicolás Seguí <sergionicolassegui@gmail.com>
2025-05-11 17:11:05 +0100

    Cambios en las directivas

--- src/poker/src/app/pages/poker/poker.component.ts
+++ src/poker/src/app/pages/poker/poker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '@/components/card/card.component';
 import { Deck } from '@/class/deck';
